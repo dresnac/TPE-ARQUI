36fc6ab394e968d42dbd4564d526d0179a518a1c
andycortese <corteseandres1@gmail.com>
2024-11-05 23:29:42 -0300

    MOD
    final changes
    
    Co-Authored-By: jeroesquivel <115410188+jeroesquivel@users.noreply.github.com>

--- x64BareBones-master/run.sh
+++ x64BareBones-master/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-qemu-system-x86_64 \
-  -hda Image/x64BareBonesImage.qcow2 \
-  -m 512 \
-  -audiodev pa,id=speaker,server=/mnt/wslg/PulseServer \
-  -machine pcspk-audiodev=speaker
+
+if [ -z "$1" ]; then
+  echo "Por favor, proporciona un tipo de SO como argumento ('MAC' o 'Ubuntu' o 'WSL')."
+  exit 1
+fi
+SO=$1
+
+if [ "$SO" == "MAC" ]; then
+  sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker
+elif [ "$SO" == "Ubuntu" ]; then
+  sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+elif [ "$SO" == "WSL" ]; then
+  sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker,server=/mnt/wslg/PulseServer -machine pcspk-audiodev=speaker
+else
+  echo "Tipo de SO no reconocido. Usa 'linux' o 'windows'."
+  exit 1
+fi
